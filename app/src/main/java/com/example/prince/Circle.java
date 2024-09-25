4d6628063e0d3d3e3117e581028d1da264063891
AwaisAliShahid <aali786181@gmail.com>
2024-09-25 16:12:20 -0600

    added Circle.java

---
+++ app/src/main/java/com/example/prince/Circle.java
@@ -0,0 +1,18 @@
+public class Circle extends Shape {
+    private double radius;
+
+    // Constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // Getter for radius
+    public double getRadius() {
+        return radius;
+    }
+
+    // Setter for radius
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
